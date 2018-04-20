e8e9fcf7cf29964444c38afb7367c025f353da82
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-04-20 10:05:30 +0200

    more some if statements

--- src/com/jeremy/Main.java
+++ src/com/jeremy/Main.java
@@ -51,10 +51,40 @@ public class Main {
             System.out.println("You got the high score!");
 
         topScore = 80;
-        int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100)
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Geater than second score and less then 100");
+        if((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of this tests is true");
 
+        int newValue = 50;
+        if(newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = false;
+        if (isCar == true)
+            System.out.println("This is not supposed to happen");
+        boolean wasCar = isCar ? true : false; //
+        if (isCar)
+            System.out.println("wasCar is true");
+
+        // 1. Create a double variable with the value 20
+        // 2. Create a second variable of type double with the value 80
+        // 3. Add both numbers up and multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that displays a message "Total was over limit"
+        //    if the remaining total (#4) is equal to 20 or less.
+
+        // Section 4 session 22
+        // 15:32
+
+        double firstOne = 20;
+        double secondOne = 80;
+        double addMultiply = (firstOne + secondOne) * 25;
+        addMultiply = addMultiply / 40;
+        if (addMultiply <= 20)
+            System.out.println("Total was over limit" + addMultiply);
+        System.out.println(addMultiply);
 
     }
 }
